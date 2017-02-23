409d044b1fef399eb08f4b7ec95c839c8e887a7c
Denis Podlesniy <haos616@gmail.com>
2017-02-23 02:54:52 +0200

    Added gosu for django image

---
+++ docker/images/django/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+USER_ID=${LOCAL_USER_ID:-1000}
+
+echo "Starting with UID : $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+export HOME=/home/user
+
+gosu user "$@"
