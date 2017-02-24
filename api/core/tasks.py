9dfe16cdf4566493f58f2a8b68c989be6070e170
Denis Podlesniy <haos616@gmail.com>
2017-02-24 03:48:00 +0200

    Add debugger rdb for celery

--- api/core/tasks.py
+++ api/core/tasks.py
@@ -1,8 +1,11 @@
 from django.core.mail import send_mail
 
+from celery.contrib import rdb
+
 from celery_app import app as celery_app
 
 
 @celery_app.task
 def email():
+    rdb.set_trace()
     send_mail('Test subject', 'Test message', 'from@example.com', ['to@example.com'])
