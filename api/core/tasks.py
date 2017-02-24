6b1a84f43f544def604b1087a9b9a88f7ffd33b9
Denis Podlesniy <haos616@gmail.com>
2017-02-24 04:00:30 +0200

    Add mailhog service and change setting

--- api/core/tasks.py
+++ api/core/tasks.py
@@ -1,11 +1,8 @@
 from django.core.mail import send_mail
 
-from celery.contrib import rdb
-
 from celery_app import app as celery_app
 
 
 @celery_app.task
 def email():
-    rdb.set_trace()
     send_mail('Test subject', 'Test message', 'from@example.com', ['to@example.com'])
